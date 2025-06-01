880cf86d3ec13aac1aac8131bd874a96ac21a3eb
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-06-01 13:32:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import scipy
+import sklearn
 
 st.title('Fractal Analysis')
 
-st.write('This is a web application for tumor fractal dimension')
+st.header('This is a web application for tumor fractal dimension')
+
+st.file_uploader('', type=['jpeg', 'jpg', 'png'])
